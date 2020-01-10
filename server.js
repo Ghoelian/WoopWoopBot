5a4bc85b24195d827b13dbf943358748764d0d17
Julian Vos <post@julianvos.nl>
2020-01-10 11:16:48 +0100

    Fixed some stuff

--- server.js
+++ server.js
@@ -18,50 +18,50 @@ app.get('/login', (req, res) => {
 })
 
 app.get('/callback', (req, res) => {
-  if (req.query.state === state) {
-    const data = JSON.stringify({
-      SPOTIFY_USER_AUTHORIZATION: req.query.code,
-      SPOTIFY_USER_AUTHORIZATION_DATE: Date.now(),
-      SPOTIFY_USER_AUTHORIZATION_EXPIRES_IN: req.query.expires_in
-    }, null, 2)
+  if (req.query.token_type === 'Bearer') {
+    if (req.query.state === state) {
+      const data = JSON.stringify({
+        SPOTIFY_USER_AUTHORIZATION: req.query.code,
+        SPOTIFY_USER_AUTHORIZATION_DATE: Date.now(),
+        SPOTIFY_USER_AUTHORIZATION_EXPIRES_IN: req.query.expires_in
+      }, null, 2)
 
-    fs.writeFileSync('auth.json', data)
+      fs.writeFileSync('auth.json', data)
 
-    request({
-      headers: {
-        Authorization: 'Basic ' + Buffer.alloc(process.env.SPOTIFY_API_SECRET).toString('base64'),
-        'Content-Type': 'application/x-www-form-urlencoded'
+      request({
+        headers: {
+          Authorization: 'Basic ' + Buffer.alloc(process.env.SPOTIFY_API_SECRET).toString('base64'),
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        method: 'POST',
+        url: 'https://accounts.spotify.com/api/token',
+        body: {
+          grant_type: 'authorization_code',
+          code: key.SPOTIFY_USER_AUTHORIZATION,
+          redirect_uri: 'http://localhost:3000/callback'
+        }
       },
-      method: 'POST',
-      url: 'https://accounts.spotify.com/api/token',
-      body: {
-        grant_type: 'authorization_code',
-        code: key.SPOTIFY_USER_AUTHORIZATION,
-        redirect_uri: 'http://localhost:3000/token'
-      }
-    },
-    (error, response, body) => {
-      if (error) {
-        console.log('We not good bois ' + error)
-      } else {
-        console.log('We good bois')
-      }
+      (error, response, body) => {
+        if (error) {
+          console.log('We not good bois ' + error)
+        } else {
+          console.log('We good bois')
+        }
+      })
+
+      res.send('Spotify authenticated.')
+    } else {
+      res.send('Invalid state parameter.')
+    }
+  } else {
+    const data = JSON.stringify({
+      SPOTIFY_USER_ACCESS: req.query.access_token,
+      SPOTIFY_USER_ACCESS_EXPIRES_IN: req.query.expires_in,
+      SPOTIFY_USER_REFRESH_TOKEN: req.query.refresh_token
     })
 
-    res.send('Spotify authenticated.')
-  } else {
-    res.send('Invalid state parameter.')
+    fs.writeFileSync('access.json', data)
   }
-})
-
-app.get('/token', (req, res) => {
-  const data = JSON.stringify({
-    SPOTIFY_USER_ACCESS: req.query.access_token,
-    SPOTIFY_USER_ACCESS_EXPIRES_IN: req.query.expires_in,
-    SPOTIFY_USER_REFRESH_TOKEN: req.query.refresh_token
-  })
-
-  fs.writeFileSync('access.json', data)
 })
 
 const listener = app.listen(process.env.PORT, () => {
